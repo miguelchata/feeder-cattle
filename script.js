373918fdd2360d48be833d9767b94edd73529045
Miguel Chata <mic.siu7@gmail.com>
2026-08-01 14:35:27 -0500

    Chagend default inicial values from base nutritional

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Base de datos nutricional (valores reales de tu primera imagen)
 const baseNutricional = [
-    { id: "avena", nombre: "Heno de avena", pt: 8.0, fdn: 63.0, precio: 0.60, inicial: 30.00 },
-    { id: "haba", nombre: "Broza de haba", pt: 9.6, fdn: 62.8, precio: 0.10, inicial: 5.00 },
-    { id: "maiz", nombre: "Maíz amarillo", pt: 9.8, fdn: 10.8, precio: 1.00, inicial: 16.67 },
-    { id: "afrecho", nombre: "Afrecho de trigo (SPT)", pt: 15.0, fdn: 42.8, precio: 1.30, inicial: 16.50 },
-    { id: "polvillo", nombre: "Polvillo de arroz", pt: 14.4, fdn: 33.0, precio: 1.00, inicial: 15.83 },
-    { id: "soya", nombre: "Torta de soya", pt: 44.0, fdn: 14.9, precio: 2.15, inicial: 15.00 },
-    { id: "sal", nombre: "Sal común", pt: 0.0, fdn: 0.0, precio: 0.20, inicial: 0.50 },
-    { id: "rocsalfos", nombre: "Rocsalfos", pt: 0.0, fdn: 0.0, precio: 2.10, inicial: 0.50 },
-    { id: "soyaintegral", nombre: "Harina integral de soya", pt: 38.0, fdn: 22.0, precio: 2.22, inicial: 5}
+    { id: "avena", nombre: "Heno de avena", pt: 8.0, fdn: 63.0, precio: 0.60, inicial: 40.0 },
+    { id: "haba", nombre: "Broza de haba", pt: 9.6, fdn: 62.8, precio: 0.10, inicial: 10.0 },
+    { id: "maiz", nombre: "Maíz amarillo", pt: 9.8, fdn: 10.8, precio: 1.00, inicial: 10.0 },
+    { id: "afrecho", nombre: "Afrecho de trigo (SPT)", pt: 15.0, fdn: 42.8, precio: 1.30, inicial: 10.0 },
+    { id: "polvillo", nombre: "Polvillo de arroz", pt: 14.4, fdn: 33.0, precio: 1.00, inicial: 10.0 },
+    { id: "soya", nombre: "Torta de soya", pt: 44.0, fdn: 14.9, precio: 2.15, inicial: 16.0 },
+    { id: "soyaintegral", nombre: "Harina integral de soya", pt: 38.0, fdn: 22.0, precio: 2.22, inicial: 6.0},
+    { id: "melaza", nombre: "Melaza", pt: 5.8, fdn: 1.0, precio: 2.0, inicial: 0},
+    { id: "sal", nombre: "Sal común", pt: 0.0, fdn: 0.0, precio: 0.20, inicial: 0.5 },
+    { id: "rocsalfos", nombre: "Rocsalfos", pt: 0.0, fdn: 0.0, precio: 2.10, inicial: 0.5 },
 ];
 
 const tbody = document.getElementById("tabla-insumos");
@@ -88,12 +89,12 @@ function calcularRacion() {
 
         // Validación visual dinámica de la Salud del Rumen (FDN)
     const statusFDN = document.getElementById("status-fdn");
-    if (aporteFDNTotal < 25.0) {
+    if (aporteFDNTotal < 33.0) {
         statusFDN.innerText = "⚠ Alerta: ¡Riesgo de Acidosis! FDN muy bajo";
         statusFDN.className = "status no-cumple";
-    } else if (aporteFDNTotal > 50.0) {
+    } else if (aporteFDNTotal > 40.0) {
         statusFDN.innerText = "⚠ Alerta: Alimento muy fibroso / Engorde lento";
-        statusFDN.className = "status no-cumple";
+        statusFDN.classNamse = "status no-cumple";
     } else {
         statusFDN.innerText = "✓ Rumen Sano e Ideal";
         statusFDN.className = "status cumple";
